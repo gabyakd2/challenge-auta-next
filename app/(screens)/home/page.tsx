1fcc4a036d478a45072121c39d7514a6f48d566d
gabyakd2 <gabrielsal00@hotmail.com>
2024-06-07 20:18:34 -0300

    fix: try 1 fix production

--- app/(screens)/home/page.tsx
+++ app/(screens)/home/page.tsx
@@ -1,13 +1,25 @@
-import { getAuth, signOut } from "firebase/auth";
-const auth = getAuth();
+"use client"
+import { useState } from "react";
+//Importaciones de firebase
+import { aplicationFirebase } from "@/app/credentials";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+const auth = getAuth(aplicationFirebase);
 signOut(auth).then(() => {
   // Sign-out successful.
 }).catch((error) => {
   console.error(error)
 });
 
-
 function HomeMain() {
+  const [username, setUsername] = useState<unknown>(null);
+onAuthStateChanged(auth, (userFirebase) => {
+  if (userFirebase) {
+    setUsername(userFirebase);
+  } else {
+    setUsername(null);
+  }
+});
+console.log(username)
   return (
     <div>
       <p>HOME</p>
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,7 @@
-"use client"
-import { useState } from "react";
-//Importaciones de firebase
-import { aplicationFirebase } from "./credentials";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
 import HomeMain from "./(screens)/home/page";
-const auth = getAuth(aplicationFirebase);
 
 export default function Home() {
-  const [username, setUsername] = useState<unknown>(null);
-  onAuthStateChanged(auth, (userFirebase) => {
-    if (userFirebase) {
-      setUsername(userFirebase);
-    } else {
-      setUsername(null);
-    }
-  });
-  console.log(username)
+
   return (
     <div>
       <HomeMain />
