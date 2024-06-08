e2138a5dc534c001b4f7b02ae5051d417dcc64fd
gabyakd2 <gabrielsal00@hotmail.com>
2024-06-08 18:22:17 -0300

    feat: create card catalogue

--- app/credentials.ts
+++ app/credentials.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,3 +21,22 @@ const firebaseConfig = {
 // Initialize Firebase
 export const aplicationFirebase = initializeApp(firebaseConfig);
 export const db = getFirestore(aplicationFirebase);
+export const storage = getStorage(aplicationFirebase);
+
+export async function uploadImageCardCars(imgCar) { //TODO TIPAR IMGCAR
+  if(!imgCar){
+    console.error("No se cargo ninguna imagen")
+  }
+  try {
+    //* Sube la imagen al storage
+    const storageRef = ref(storage, `imagesCardCars/${imgCar.name}`)
+    await uploadBytes(storageRef, imgCar)
+    console.log("La imagen se cargo con éxito")
+
+    //* Obtener el link de descarga de la imagen
+    const downloadUrl = await getDownloadURL(storageRef);
+    return downloadUrl
+  } catch (error) {
+    console.error("Error al cargar la imagen:", error)
+  }
+}
