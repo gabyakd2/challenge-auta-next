fb43fabb5b9f7d746685310ee072bc653a329256
gabyakd2 <gabrielsal00@hotmail.com>
2024-06-08 22:11:24 -0300

    refactor and add: hook useSignOut added, refactor form add car

--- app/(screens)/home/page.tsx
+++ app/(screens)/home/page.tsx
@@ -2,16 +2,19 @@
 import HeaderHome from "./components/headerHome/HeaderHome";
 import CardList from "./components/cardList/CardList";
 import { useAuth } from "@/app/hook/useAuth";
+import { useSignOut } from "@/app/hook/useSignOut";
 
 function HomeMain() {
   const {username} = useAuth();
+  const { error, handleSignOut } = useSignOut();
 
 console.log(username)
   return (
     <div>
       <HeaderHome />
       <CardList />
-      <button>Logout</button>
+      <button onClick={handleSignOut}>Logout</button>
+      {error && <p>Error: {error}</p>}
     </div>
   )
 }
